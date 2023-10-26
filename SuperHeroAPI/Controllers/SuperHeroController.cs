4d9680b2d66f52d5293301f356aae6a20fbee35d
HuynhBaoNgoc <hiudahuynh@gmail.com>
2023-10-26 17:43:42 +0700

    add post put delete method

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -15,7 +15,12 @@ namespace SuperHeroAPI.Controllers
                     FirstName = "Peter",
                     LastName = "Parker",
                     Place = "New York City"
-                }
+                },
+                new SuperHero { Id = 2,
+                    Name = "Iron man",
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    Place = "Long Island"}
             };
 
         [HttpGet]
@@ -24,10 +29,42 @@ namespace SuperHeroAPI.Controllers
             return Ok(heroes);
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> Get(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return BadRequest("Hero not found.");
+            return Ok(hero);
+        }
+
         [HttpPost]
         public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
         {
             heroes.Add(hero);
+            return Ok(heroes);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
+        {
+            var hero = heroes.Find(h => h.Id == request.Id);
+            if (hero == null)
+                return BadRequest("Hero not found.");
+            hero.Name = request.Name;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.Place = request.Place;
+            return Ok(heroes);  
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> Delete(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return BadRequest("Hero not found.");
+            heroes.Remove(hero);
             return Ok(heroes);
         }
     }
